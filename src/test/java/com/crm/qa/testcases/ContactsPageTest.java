2b5964ab71f5486b381293e67cc050abe38aa7c1
Sumit K Sharma <sumitksharma@Sumits-iMac.local>
2017-12-06 15:24:32 +0530

    removed unused import from ContactsPageTest

--- src/test/java/com/crm/qa/testcases/ContactsPageTest.java
+++ src/test/java/com/crm/qa/testcases/ContactsPageTest.java
@@ -8,7 +8,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.ContactsPage;
 import com.crm.qa.pages.HomePage;
@@ -26,7 +25,6 @@ public class ContactsPageTest extends TestBase{
 	public ContactsPageTest(){
 		super();
 	}
-
 	@BeforeMethod
 	public void setUp() throws InterruptedException{
 		initialization();
